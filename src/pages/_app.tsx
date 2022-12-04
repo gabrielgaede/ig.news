45dd055b10bd685ecd9c5dc11bdf0dce46cfdc3d
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2022-12-04 18:24:46 -0300

    login with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionProvider as NextAuthProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />    
-    </>
+    </NextAuthProvider>
   )
 }
