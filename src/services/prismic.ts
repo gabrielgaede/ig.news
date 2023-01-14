02bd4c47d91a6a1654caa8cfd0b4bd87cf648ce8
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2023-01-14 17:48:19 -0300

    fetching posts

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,13 +1,22 @@
-import Prismic from '@prismicio/client';
+import * as prismic from '@prismicio/client'
 
-export function getPrismicClient(req?: unknown) {
-    const prismic = Prismic.createClient(
-        process.env.PRISMIC_ENDPOINT!,
-        {
-            req: req,
-            accessToken: process.env.PRISMIC_ACCESS_TOKEN!
-        }
-    )
+// Fill in your repository name
+export const repositoryName = process.env.PRISMIC_ENDPOINT!;
 
-    return prismic;
-}
+export const client = prismic.createClient(repositoryName, {
+  // If your repository is private, add an access token
+  accessToken: process.env.PRISMIC_ACCESS_TOKEN!,
+
+  // This defines how you will structure URL paths in your project.
+  // Update the types to match the Custom Types in your project, and edit
+  // the paths to match the routing in your project.
+  //
+  // If you are not using a router in your project, you can change this
+  // to an empty array or remove the option entirely.
+  routes: [
+    {
+      type: 'post',
+      path: '/',
+    },
+  ],
+})
